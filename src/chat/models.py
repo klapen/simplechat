93f310be71f0ec751035bf17a4d6bf6f1c6bf91b
osaavedra <osaavedra@bora.com.co>
2019-05-24 15:18:34 -0500

    Fix bug - Fetch messages timestamp order

--- src/chat/models.py
+++ src/chat/models.py
@@ -13,4 +13,4 @@ class Message(models.Model):
         return self.author.username
 
     def last_50_messages(room):
-        return Message.objects.order_by('-timestamp').filter(room=room)[:50]
+        return Message.objects.order_by('timestamp').filter(room=room)[:50]
